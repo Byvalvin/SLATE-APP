1a5a6e1499ff67e562c8810001f0cffe45d09be5
cruzmart <cruzmart@ualberta.ca>
2025-05-12 17:36:44 -0600

    commonJS vs es modules

--- Slate-Backend/server.js
+++ Slate-Backend/server.js
@@ -32,4 +32,5 @@ app.use(`${baseURL}/auth`, authRoutes);
 
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-export default app;
+
+module.exports = app;  // Change here
