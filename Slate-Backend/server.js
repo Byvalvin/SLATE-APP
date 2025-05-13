ea356242dfa8119afa973f320172cdbfec0c62c3
cruzmart <cruzmart@ualberta.ca>
2025-05-12 19:36:54 -0600

    t

--- Slate-Backend/server.js
+++ Slate-Backend/server.js
@@ -5,8 +5,8 @@ const authRoutes = require('./routes/auth');
 
 // db setup
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
   dbName: 'Slate'  // ✅ this explicitly tells Mongoose to use "Slate" DB
 })
 .then(() => console.log('MongoDB connected to Slate DB'))
