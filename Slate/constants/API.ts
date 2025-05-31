356a0caadeb1b910a2c66e9c8ad4c6d8ac9d829f
joyce omamuli <joycemacbookair2@joyces-MacBook-Air2.local>
2025-05-30 23:36:57 -0600

    another one

--- Slate/constants/API.ts
+++ Slate/constants/API.ts
@@ -1,5 +1,6 @@
 export const servers = [
     'http://10.0.0.210:3000', 
-    'https://slate-app-psi.vercel.app'
+    'https://slate-app-psi.vercel.app',
+    'https://slate-backend.vercel.app'
   ]
   
